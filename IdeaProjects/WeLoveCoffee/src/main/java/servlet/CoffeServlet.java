0c85cb7764fc55a7e1337f9d4dc65f285132fdb2
Guilherme Ferreira <guilherme.ea16013@gmail.com>
2023-04-08 22:53:21 -0300

    Criando as classe padrão MVS

--- IdeaProjects/WeLoveCoffee/src/main/java/servlet/CoffeServlet.java
+++ IdeaProjects/WeLoveCoffee/src/main/java/servlet/CoffeServlet.java
@@ -1,3 +1,8 @@
+package servlet;
+
+import dao.Dao;
+import model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,15 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/screen-one")
-public class ScreenOneServlet extends HttpServlet {
+@WebServlet("/create-user")
+public class CoffeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nomeLogin = request.getParameter("login-screen");
 
-        System.out.println(nomeLogin);
+        String userEmail = request.getParameter("usuario");
+        String userPassword = request.getParameter("password");
+
+        User user = new User();
+        user.setEmail(userEmail);
+        user.setPassword(userPassword);
+        new Dao().createUser(user);
+
+        System.out.println(userEmail + " - " + userPassword);
 
         request.getRequestDispatcher("index.html").forward(request, response);
+
     }
+
 }
